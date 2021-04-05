fe89918c833be7493bbd7dffa78d860950c43f76
Agustin barajas <agustinbvaldivia@gmail.com>
2021-04-05 14:20:51 -0600

    Add authentication with devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
-    # before_action :find_article, only: [:show, :edit, :update, :delete]
     before_action :find_article, except: [:new, :create, :index]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :delete]
 
     def index
         @articles = Article.all
